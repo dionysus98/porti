644ad7f6dccd8159e4e435ed6cae3a55ce71da50
Avinash S <avinashthebigboss@gmail.com>
2025-06-23 16:09:36 +0530

    fix | release | paths

--- resources/public/js/cljs-runtime/replicant.hiccup_headers.js
+++
@@ -1,3 +0,0 @@
-goog.provide('replicant.hiccup_headers');
-
-//# sourceMappingURL=replicant.hiccup_headers.js.map
--- resources/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { proti.dev.main_BANG_(); } catch (e) { console.error("An error occurred when calling (proti.dev/main!)"); console.error(e); }
